781e2e4cca08d1b5e2697257b3749f46f1a7b27c
girvel <widauka@ya.ru>
2024-08-05 12:07:46 +0500

    Userdata!

--- fallen_tcod.c
+++ fallen_tcod.c
@@ -1,5 +1,11 @@
 #include <lua.h>
 #include <lauxlib.h>
+//#include "libtcod/fov.h"
+
+typedef struct {
+    int x;
+    int y;
+} Vector;
 
 static int l_add(lua_State *L) {
     int a = luaL_checkinteger(L, 1);
@@ -10,12 +16,43 @@ static int l_add(lua_State *L) {
     return 1;
 }
 
+// static int l_create_map(lua_State *L) {
+//     int w = luaL_checkint(L, 1);
+//     int h = luaL_checkint(L, 2);
+// 
+//     TCOD_Map *map = (TCOD_Map *)lua_newuserdata(L, sizeof(TCOD_Map))
+//     map = TCOD_map_new(w, h);
+//     return 1;
+// }
+
+static int l_create_vector(lua_State *L) {
+    int x = luaL_checkint(L, 1);
+    int y = luaL_checkint(L, 2);
+
+    Vector *v = (Vector *)lua_newuserdata(L, sizeof(Vector));
+    luaL_getmetatable(L, "Vector");
+    lua_setmetatable(L, -2);
+    v->x = x;
+    v->y = y;
+    return 1;
+}
+
+static int l_get_x(lua_State *L) {
+    luaL_checktype(L, 1, LUA_TUSERDATA);
+    Vector *v = (Vector *)luaL_checkudata(L, 1, "Vector");
+    lua_pushinteger(L, v->x);
+    return 1;
+}
+
 static const struct luaL_Reg fallen_tcod[] = {
     {"add", l_add},
+    {"create_vector", l_create_vector},
+    {"get_x", l_get_x},
     {NULL, NULL},
 };
 
 int luaopen_fallen_tcod(lua_State *L) {
+    luaL_newmetatable(L, "Vector");
     luaL_register(L, "fallen_tcod", fallen_tcod);
     return 1;
 }
--- test.lua
+++ test.lua
@@ -2,4 +2,6 @@ local tcod = require("fallen_tcod")
 
 print("-- running tests --")
 assert(tcod.add(2, 2) == 4, "2 + 2 should be equal 4")
+assert(tcod.get_x(tcod.create_vector(2, 0)) == 2, "userdata should work")
+-- assert(tcod.create_map(2, 2), "create_map should create some object")
 print("Success!")
